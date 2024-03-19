4d21613f9532502c0b8bd48a458b664e7cdc92e0
Nozarashi12 <88560863+Nozarashi12@users.noreply.github.com>
2024-03-19 15:28:38 +0530

    database

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,19 @@
-import Home from './Components/Home'
+import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom'
 import './App.css'
-import Signup from './Components/Signup'
+import Home from './Components/Home';
+import Signup from './Components/Signup';
+import Loginpg from './Components/Loginpg';
 
 function App() {
-
   return (
-    <>
-      {/* <Home/> */}
-      <Signup/>
-    </>
-  )
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/Signup' element={<Signup/>}/>
+      <Route path='/Loginpg' element={<Loginpg/>}/>
+
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import BG from '../assets/main.png';
 
 function Home() {
+  useEffect(() => {
+    const glitchText = document.querySelector('.glitch');
+    glitchText.addEventListener('mouseover', () => {
+      glitchText.classList.add('glitch-active');
+      setTimeout(() => {
+        glitchText.classList.remove('glitch-active');
+      }, 1500);
+    });
+  }, []);
+
   return (
     <div className='bg-black h-screen w-screen' style={{backgroundImage:`url(${BG})`}}>
       <div className='pt-64 pl-64 '>
-        <h2 className='logo text-white glitch' data-text='Neo Tokyo'>Neo<br/> Tokyo</h2>
+      <h2 className='logo text-white'>
+  <span className='glitch' data-text='Neo Tokyo'>Neo</span><br/> Tokyo
+</h2>
+
         <p className='text-white  text-4xl para'>Leveling up creativity <br/> One pixel at a time</p>
         <button className='border border-white rounded-lg bg-black w-40 h-14 text-white mt-20 text-2xl txt'>Get Started</button>
       </div>
