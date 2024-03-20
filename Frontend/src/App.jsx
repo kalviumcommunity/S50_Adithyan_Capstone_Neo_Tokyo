bb93e9c27e2c733a8b17abac86c0c6581ae8a295
Nozarashi12 <88560863+Nozarashi12@users.noreply.github.com>
2024-03-20 17:04:01 +0530

    home

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Route, Routes, Link } from 'react-router-dom'
+import { Route,Routes} from 'react-router-dom'
 import './App.css'
 import Home from './Components/Home';
 import Signup from './Components/Signup';
 import Loginpg from './Components/Loginpg';
-
+import Landing from './Components/Landing';
 function App() {
   return (
     <Routes>
+      <Route path='/Landing' element={<Landing/>}/>
       <Route path='/' element={<Home/>}/>
       <Route path='/Signup' element={<Signup/>}/>
       <Route path='/Loginpg' element={<Loginpg/>}/>
